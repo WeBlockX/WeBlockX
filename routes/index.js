53f7278e347979151f50ca3d295565d163a1abc7
dvsoo <qpzm2480@naver.com>
2018-10-30 09:01:10 +0900

    include file add

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WeBlock X' });
 });
+
+router.get('/academy', function(req, res){
+  res.render('academy', {title: 'WeBlock X Academy'})
+})
+
+router.get('/community', function(req, res){
+  res.render('community', {title: 'WeBlock X Community'})
+})
+
 
 module.exports = router;
