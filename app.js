e5c3e259eb211d938dc7e4be7b98bd5e3ef67efd
dvsoo <qpzm2480@naver.com>
2018-10-31 19:53:34 +0900

    router setting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -36,6 +36,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+})
 
 module.exports = app;
